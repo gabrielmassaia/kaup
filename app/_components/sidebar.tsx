d52f6b5cef8286254a4c42f95ecb97e62be7a880
gabrielmassaia <gabrielmassaia50@gmail.com>
2025-08-06 21:05:18 -0300

    feat: atualizar componente Sidebar e adicionar página de Vendas
    - Substituído botões estáticos por um componente SidebarButton com ícones.
    - Removido arquivo de layout de produtos que não estava em uso.
    - Adicionada nova página de Vendas com estrutura básica.

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,13 +1,31 @@
+import {
+  LayoutDashboard,
+  ShoppingBasketIcon,
+  ShoppingCartIcon,
+} from "lucide-react";
+import SidebarButton from "./sidebar-button";
+
 const SideBar = () => {
   return (
     <div className="w-64 bg-white">
       <div className="px-8 py-6">
         <h1 className="text-2xl font-bold">Kaup</h1>
       </div>
-      <div className="gp-2 flex flex-col p-2">
-        <button className="px-6 py-3">Dashboard</button>
-        <button className="px-6 py-3">Produtos</button>
-        <button className="px-6 py-3">Vendas</button>
+      <div className="flex flex-col gap-2 p-2">
+        <SidebarButton href="/">
+          <LayoutDashboard size={20} />
+          Dashboard
+        </SidebarButton>
+
+        <SidebarButton href="/products">
+          <ShoppingBasketIcon size={20} />
+          Produtos
+        </SidebarButton>
+
+        <SidebarButton href="/sales">
+          <ShoppingCartIcon size={20} />
+          Vendas
+        </SidebarButton>
       </div>
     </div>
   );
